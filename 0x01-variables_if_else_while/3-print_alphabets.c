a531f5ad7c071914e14456430d407df7817c47eb
muchendu221 <muchendu22@gmail.com>
2022-12-09 06:59:15 -0800

    Print lower case and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,10 +11,14 @@ int main(void)
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
+	  {
 		putchar(low);
+	  }
 	for (low = 'A'; low <= 'Z'; low++)
+	  {
 		putchar(low);
-		putchar('\n');
+	  }
+	putchar('\n');
 
 	return (0);
 }
