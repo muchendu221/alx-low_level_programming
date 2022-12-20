6212c0149782b1705894918393390338e7b91dbf
muchendu221 <muchendu22@gmail.com>
2022-12-20 13:18:55 -0800

    Swap two numbers given their pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * swap_int swaps two number given the pointers
- *
+ * swap_int -  swaps two number given the pointers
+ *@a: number to swapped
+ *@b: pointert of number to be swapped
  * Return 0.
  */
 void swap_int(int *a, int *b)
 {
 	int temp;
 
-	temp = * a;
+	temp = *a;
 	*a = *b;
 	*b = temp;
 }
