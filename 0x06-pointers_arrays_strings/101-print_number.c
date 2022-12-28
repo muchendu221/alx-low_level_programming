5495d5816964bdf57eff10b3d52694b822a665fb
muchendu221 <muchendu22@gmail.com>
2022-12-28 09:30:09 -0800

    correction for 102

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,15 +10,14 @@ void print_number(int n)
 {
 	unsigned int x;
 
+	x = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		x = -n;
 	}
 
-	x = n;
-
-	if (x / 10)
+	if (x / 10 != 0)
 		print_number(x / 10);
 	_putchar(x % 10 + '0';
 }
