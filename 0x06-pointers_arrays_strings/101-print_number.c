35f0196185bec5c72d447d2dda13b374d4e572ab
muchendu221 <muchendu22@gmail.com>
2022-12-28 09:38:52 -0800

    corrections for 102

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,4 @@
-#incliude "main.h"
+#include "main.h"
 
 /**
  *print_number - it prints integer
@@ -19,5 +19,5 @@ void print_number(int n)
 
 	if (x / 10 != 0)
 		print_number(x / 10);
-	_putchar(x % 10 + '0';
+	_putchar(x % 10 + '0');
 }
