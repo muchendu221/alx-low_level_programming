7d0ac4b995311a0a6d6be8522a61ee2e6f138adf
muchendu221 <muchendu22@gmail.com>
2023-01-12 00:39:09 -0800

    hot fix

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -11,15 +11,5 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
-		while (*s)
-		{
-			for (i = 0; accept[i]; i++)
-			{
-				if (*s == accept[i])
-					return (s);
-			}
-			s++;
-		}
-	return (NULL);
+	
 }
