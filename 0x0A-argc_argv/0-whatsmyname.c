5e92b2f7b964cfbd73c39832362df3f11a2d35ff
muchendu221 <muchendu22@gmail.com>
2023-01-12 02:49:43 -0800

    Hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,18 +3,17 @@
 
 /**
  * main - Prints new name
- * @argc - parameter count
- * @argv - Parameter array
+ * @argc: parameter count
+ * @argv: Parameter array
  * Description: A Program that prints its name
  * Return: Always 0
  */
 
 int main(int argc, char **argv)
 {
-	printf("%s\n", argv[0]);
-/*	while (argc--)
+	while (argc--)
 	{
 		printf("%s\n", *argv++);
-		}*/
+		}
 	return (0);
 }
