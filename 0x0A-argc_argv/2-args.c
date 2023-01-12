ac732e79d87b6c0742c04edd4640209740ecfab2
muchendu221 <muchendu22@gmail.com>
2023-01-12 03:00:28 -0800

    Print the arguements passed on to a function

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints all the arguements passed to a function
+ * @argc: The parameter count
+ * @argv: The parameter array
+ *
+ *Description: Prints the arguement passed to a function, each on its own line
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
