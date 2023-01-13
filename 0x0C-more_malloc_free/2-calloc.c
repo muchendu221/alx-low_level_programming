86775fed03f80f53aef2937ddc1d521fa7c91693
muchendu221 <muchendu22@gmail.com>
2023-01-13 03:55:50 -0800

    hot fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,15 +13,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int i, d;
 
-	d = nmemd * size;
+	d = nmemb * size;
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 	p = malloc(d);
 	if (p == NULL)
 		return (NULL);
-	for (i = 0, i < d; i++)
+	for (i = 0; i < d; i++)
 	{
-		p[i] = 0
+		p[i] = 0;
 			}
 	return (p);
 }
