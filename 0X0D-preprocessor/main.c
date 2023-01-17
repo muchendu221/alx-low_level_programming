80c8877aae08a183a6daee5bd154cb3adaf9ed3c
muchendu221 <muchendu22@gmail.com>
2023-01-17 00:24:38 -0800

    Created a macro SIZE

---
+++ 0X0D-preprocessor/main.c
@@ -0,0 +1,15 @@
+#include "0-object_like_macro.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ *Return: Always return 0;
+ */
+int main(void)
+{
+	int s;
+	
+	s = SIZE + 98;
+	printf("%d\n", s);
+	return (0);
+}
