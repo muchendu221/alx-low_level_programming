a9117ced7f22c09d1da6de2bb1820946b18aec80
muchendu221 <muchendu22@gmail.com>
2023-01-17 02:29:50 -0800

    First Struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new datatype for dog
+ *@name: anem of dog
+ *@age: the age of the dog
+ *@owner: The owner of the dog
+ *
+ * Return: Always 0
+ */
+struct dog
+{
+	char *type;
+	float age;
+	char *owner;
+};
+#endif
