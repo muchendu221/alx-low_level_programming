bc1e1781089ea4b1b799ee47dd71351e9925e208
muchendu221 <muchendu22@gmail.com>
2023-01-17 03:02:31 -0800

    hot fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,7 +8,7 @@
  *@owner: owner of Dog.
  * Return: Always 0.
  */
-void init_dog(struct dog *x, char *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
 	{
