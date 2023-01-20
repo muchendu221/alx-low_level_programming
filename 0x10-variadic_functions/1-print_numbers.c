2c2fcdc70934cecd2fc0b056107aeb1ee61c912f
muchendu221 <muchendu22@gmail.com>
2023-01-20 01:38:42 -0800

    Fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include <stdarg.h>
 #include <stddef.h>
+#include <stdio.h>
 /**
  * print_numbers - Prints numbers
  * @separator: special character for separation
@@ -15,13 +16,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
+		printf("%d", va_arg(ap, int));
 		if (separator != NULL)
 		{
 			if (i < n - 1)
 				printf("%s", separator);
 		}
 	}
-	putchar('\n');
+	printf("\n");
 	va_end(ap);
 }
