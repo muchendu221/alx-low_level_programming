26ab36b4b48b3b299565950c0e422846da1ceaac
muchendu221 <muchendu22@gmail.com>
2023-01-20 04:11:36 -0800

    fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,10 +5,10 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 int _putchar(char c);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_char(va_list);
-void print_int(va_list);
-void print_float(va_list);
-void print_string(va_list);
+void print_c(va_list);
+void print_i(va_list);
+void print_ft(va_list);
+void print_s(va_list);
 
 /**
  * struct format_t - entry to types
@@ -16,9 +16,9 @@ void print_string(va_list);
  *@t: type of variable
  *@f: function to print type of variable
  */
-typedef struct format_t
+typedef struct print_t
 {
 	char *t;
 	void (*f)(va_list);
-} format_t;
+} print_t;
 #endif
