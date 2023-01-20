fbdec1b0a22a76479619161fa467d0b5c97f6034
muchendu221 <muchendu22@gmail.com>
2023-01-20 04:13:44 -0800

    Hot fix

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,82 +1,92 @@
-#include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
 
 /**
- * print_char - entry va_list like type
- * @c: get va_list
+ * print_c - print a char
+ * @c: char to print
  *
+ * Return: void
  */
-void print_char(va_list c)
+void print_c(va_list c)
 {
 	printf("%c", va_arg(c, int));
 }
+
 /**
- * print_int - entry point va_list type
- * @i: va_list
+ * print_s - prints a string
+ * @s: string to print
+ *
+ * Return: void
  */
-void print_int(va_list i)
-{
-	printf("%i", va_arg(i, int));
-}
-/**
- * print_float - entry point for type
- *@f: get va_list
- */
-void print_float(va_list f)
-{
-	printf("%f", va_arg(f, double));
-}
-/**
- * print_string - entry point
- * @s: get va_list
- */
-void print_string(va_list s)
+void print_s(va_list s)
 {
 	char *str = va_arg(s, char *);
 
-	if (str != NULL)
-	{
-		printf("%s", str);
-		return;
-	}
-	printf("(nil)");
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
 }
+
 /**
- * print_all - entry arguements
- * @format: get quantity arguments
+ * print_i - prints an int
+ * @i: int to print
+ *
+ * Return: void
+ */
+void print_i(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * print_f - prints a float
+ * @f: float to print
+ *
+ * Return: void
+ */
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
-	format_t type[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string},
+	unsigned int i, j;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"f", print_f},
 		{NULL, NULL}
 	};
-	va_list ap;
-	int i, j;
-	char *sp = "";
+	va_list valist;
+	char *separator = "";
 
-	va_start(ap, format);
+	va_start(valist, format);
 	i = 0;
 	while (format && format[i])
 	{
 		j = 0;
-		while (j < 4)
+		while (p[j].t != NULL)
 		{
-			if (type[j].t[0] == format[i])
+			if (*(p[j].t) == format[i])
 			{
-				printf("%s", sp);
-				type[j].f(ap);
-				sp = ", ";
+				printf("%s", separator);
+				p[j].f(valist);
+				separator = ", ";
 				break;
 			}
 			j++;
 		}
 		i++;
 	}
+	va_end(valist);
 	printf("\n");
-	va_end(ap);
 }
