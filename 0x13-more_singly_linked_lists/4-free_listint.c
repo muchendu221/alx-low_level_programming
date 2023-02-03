e0f7a36dce7a90de83a14629753c878254f26ab4
muchendu221 <muchendu22@gmail.com>
2023-02-03 03:23:53 -0800

    free a list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint - entry point
+ * @head: head of list to be freed
+ *
+ *
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
