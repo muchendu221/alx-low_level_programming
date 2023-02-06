1d6158220aa2a26aaef27d3cc4b624bfea3185cf
muchendu221 <muchendu22@gmail.com>
2023-02-06 05:19:13 -0800

    Hot fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * binary_to_uint - entry point
- * @b: - pointer to the start of binary string
- * Return: The converted number or 0 if b is NULL or if there are chars tha are
- * not 0 or 1
+ * binary_to_uint - Converters a binary number to an unsigned integer
+ * @b: String representing a binary number
+ * Return: The converted number or 0 if b is NULL or if there are chars
+ * that are not 0 or 1
  */
 unsigned int binary_to_uint(const char *b)
 {
@@ -20,19 +20,19 @@ unsigned int binary_to_uint(const char *b)
 	pow = 1;
 	if (b == NULL)
 		return (0);
-	for (len = 0; b[len] != '\0'; len++) /*Checking the length of b*/
+	for (len = 0; b[len] != '\0'; len++) /*Getting the length of b num*/
 		;
-	if (len == 1 && (b[0] == '0' || b[0] == '1')) /* checking for single numbs*/
+	if (len == 1 && (b[0] == '0' || b[0] == '1')) /*Checking single nums*/
 		return ((b[0] - 48));
-	for (i = 0; b[i] != '\0'; i++)/* iterating through the list*/
+	for (i = 0; b[i] != '\0'; i++) /*Iterating through the string*/
 	{
-		if (b[i] != '0' && b[1] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		for (j = len - 1; j > 0; j--) /*calculating the exponent*/
+		for (j = len - 1; j > 0; j--) /*Calculating the exponent*/
 			pow = pow * base;
-		sum = sum + (pow * (b[i] - 48));
+		sum = sum + (pow * (b[i] - 48)); /*Adding the number*/
 		len--;
-		pow = 1;/* reset exponent*/
+		pow = 1; /*Reseting the exponent*/
 	}
 	return (sum);
 }
