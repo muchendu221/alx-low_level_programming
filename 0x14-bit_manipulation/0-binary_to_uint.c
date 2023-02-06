3ccb48e9726e4c0aa7d82da696a7704f31d2f0e0
muchendu221 <muchendu22@gmail.com>
2023-02-06 05:27:47 -0800

    fixec code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,38 +1,59 @@
 #include "main.h"
-
 /**
- * binary_to_uint - Converters a binary number to an unsigned integer
- * @b: String representing a binary number
- * Return: The converted number or 0 if b is NULL or if there are chars
- * that are not 0 or 1
+ * binary_to_uint - entry point
+ * @b: string with information about binary number
+ * Return: a decimal number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len;
-	unsigned int i;
-	unsigned int j;
-	unsigned int sum;
-	unsigned int pow;
-	int base;
+	int i = 0, exp = 0;
+	unsigned int res = 0;
 
-	base = 2;
-	sum = 0;
-	pow = 1;
 	if (b == NULL)
-		return (0);
-	for (len = 0; b[len] != '\0'; len++) /*Getting the length of b num*/
-		;
-	if (len == 1 && (b[0] == '0' || b[0] == '1')) /*Checking single nums*/
-		return ((b[0] - 48));
-	for (i = 0; b[i] != '\0'; i++) /*Iterating through the string*/
+		return (res);
+
+	while (b[i])
+		i++;
+
+	for (i -= 1; i >= 0; i--)
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		for (j = len - 1; j > 0; j--) /*Calculating the exponent*/
-			pow = pow * base;
-		sum = sum + (pow * (b[i] - 48)); /*Adding the number*/
-		len--;
-		pow = 1; /*Reseting the exponent*/
+		if (b[i] == '1' || b[i] == '0')
+		{
+			if (b[i] == '1')
+			{
+				res += _pow(2, exp);
+				exp++;
+			}
+			else
+			{
+				exp++;
+			}
+		}
+		else
+		{
+			res = 0;
+			return (res);
+		}
 	}
-	return (sum);
+	return (res);
+
+}
+/**
+ * _pow - entry point
+ * @base: decimal base number
+ * @exp: decimal expone number
+ * Return: power of number
+ */
+int _pow(int base, int exp)
+{
+	int i = 0, res = 1;
+
+	if (exp == 0)
+		return (res);
+	while (i < exp)
+	{
+		res *= base;
+		i++;
+	}
+	return (res);
 }
