e29f4fd7cbfcb915f51f68a5be268284242f5686
muchendu221 <muchendu22@gmail.com>
2023-02-09 03:05:41 -0800

    Fixed issues with 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,20 @@
 #include "main.h"
+/**
+ *_strlen - entry point
+ * @str: string whose lenth we are after
+ *
+ * Return: lenght of the string or -1 in case str is NULL
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+	while (*str++)
+		++len;
+	return (len);
+}
 
 /**
  * append_text_to_file - Entry Point
@@ -8,33 +24,19 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, wr, i = 0;
+	int file;
+	ssize_t b_written = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	file = open(filename, O_RDWR | O_APPEND);
-	if (file == -1)
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file < 0)
 		return (-1);
-
-
-
-	while (text_content[i])
-		i++;
-
-	if (text_content == NULL)
-	{
-		close(file);
-		return (1);
-	}
-	else
-	{
-		wr = write(file, text_content, i);
-	}
-
-	if (wr == -1)
-		return (-1);
-
+	if (text_content)
+		b_written = write(file, text_content, _strlen(text_content));
 	close(file);
-	return (1);
+	if (b_written < 0)
+		return (-1);
+	return (0);
 }
