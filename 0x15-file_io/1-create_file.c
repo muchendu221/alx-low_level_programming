ea58752878c2007ab18af53da227e61039380e61
muchendu221 <muchendu22@gmail.com>
2023-02-09 02:36:29 -0800

    edited task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,20 @@
 #include "main.h"
+
+/**
+ * _strlen - compute the length of NULL-terminated string
+ * @str: The string to get length
+ * Return: Length of the string or -1 is string is NULL
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+	while (*str++)
+		++len;
+	return (len);
+}
 /**
  * create_file - Entry Point
  * @filename: file name
@@ -7,28 +23,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, i = 0;
+	int file;
+	ssize_t b_written;
 
 	if (filename == NULL)
 		return (-1);
 
 	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (file == -1)
+	if (file < 0)
 		return (-1);
 
-	while (text_content[i])
-		i++;
-
-	if (text_content == NULL)
-	{
-		close(file);
-		return (-1);
-	}
-	else
-	{
-		write(file, text_content, i);
-	}
-
+	if (text_content)
+		b_written = write(file, text_content, _strlen(text_content));
 	close(file);
+
+	if (b_written < 0)
+		return (-1);
 	return (1);
 }
